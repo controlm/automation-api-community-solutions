b56fb9f7857c1ccc4673cd43324076ea667071f8
Joe Goldberg <joe_goldberg@bmc.com>
2018-11-26 21:19:19 -0500

    Updated Lambda function to correctly process SNS events

--- 311-aws-lambda-invoke-controlm-services/ctmEventHandler.py
+++ 311-aws-lambda-invoke-controlm-services/ctmEventHandler.py
@@ -11,30 +11,57 @@ verbose = False
 verify_certs = False
 ctmresponse = 200
 s3res = boto3.resource('s3')
+CTMEnvironment = 'none'
+CTMServer = 'none'
+ReqType = 'none'
+Jobname = 'none'
+Folder = 'none'
+Template = 'none'
+CTMEvent = 'none'
+CTMOdate = 'none'
+S3WatchBucket = 'none'
+S3WatchFile = 'none'
 
 def lambda_handler(event, context):
-    # print("ctmEventHandler received event: " + json.dumps(event, indent=2))
+    if verbose:
+        print("ctmEventHandler received event: " + json.dumps(event, indent=2))
+        
+    try:
+        eventSource = event['Records'][0]['EventSource']
+        if eventSource == 'aws:sns':
+            SNSMessage = event['Records'][0]['Sns']['Message']
+            event = json.loads(SNSMessage)
+        else:
+            print("Uppercase EventSource but not an SNS version 1.0 event")    
+    except:
+        print("Not an SNS version 1.0 event")
+        
     eventSource = event['Records'][0]['eventSource']
+    if verbose:
+        print("Processing this event: " + json.dumps(event, indent=2))
     
     if eventSource == 'aws:sqs':
-        ctmresponse = sqs_event(event)
+        ctmreq = sqs_event(event)
+        ctmresponse = do_request(ctmreq)
     elif eventSource == 'aws:sns':
-        ctmresponse = sns_event(event)
+        ctmreq = sns_event(event)
+        ctmresponse = do_request(ctmreq)
     elif eventSource == 'aws:s3':
-        ctmresponse = s3_event(event)
+        ctmreq = s3_event(event)
+        ctmresponse = do_request(ctmreq)
     else:
         print("Unexpected event type: " + eventSource)
         ctmresponse = 400
-	  
+    
     response = {
         "statusCode": ctmresponse,
         "body": json.dumps(event)
     }
     
     return response
-	
+    
 def sqs_event(event):
-    response = 200
+    global CTMEnvironment, CTMServer, ReqType, Jobname, Folder, Template, CTMEvent, CTMOdate
     msgbody = event['Records'][0]['body']
     if verbose:
         print("Message body: " + msgbody)
@@ -47,29 +74,93 @@ def sqs_event(event):
     ReqType = msgpayload['Request'][0]['RequestAttributes']['Type']
     Jobname = msgpayload['Request'][0]['RequestAttributes']['Jobname']
     Folder = msgpayload['Request'][0]['RequestAttributes']['Folder']
-    #print("Environment: " + CTMEnvironment + " - Control-M Server: " + CTMServer + " - Request type: " + ReqType + " - Jobname: " + Jobname + " - Folder: " + Folder)
+    if verbose:
+        print("Environment: " + CTMEnvironment + " - Control-M Server: " + CTMServer + " - Request type: " + ReqType + " - Jobname: " + Jobname + " - Folder: " + Folder)
+    
+    if ReqType == 'createjob':
+        Template = msgpayload['Request'][0]['RequestAttributes']['Template']
+        
+    ctmrequest_args = collections.namedtuple('Ctmrequest_Args', ['CTMEnvironment', 'CTMServer', 'ReqType', 'Jobname', 'Folder', 'Template', 'CTMEvent', 'CTMOdate', 'S3WatchBucket', 'S3WatchFile'])
+    ctmreq = ctmrequest_args(CTMEnvironment, CTMServer, ReqType, Jobname, Folder, Template, CTMEvent, CTMOdate, S3WatchBucket, S3WatchFile)
+        
+    return ctmreq
+    
+def sns_event(event):
+    global CTMEnvironment, ReqType, Jobname, Folder, Template, Event, CTMOdate
+
+    return
+    
+def s3_event(event):
+    global CTMEnvironment, ReqType, Jobname, Folder, Template, CTMEvent, CTMOdate
+    S3WatchBucket = event['Records'][0]['s3']['bucket']['name']
+    S3WatchFile = event['Records'][0]['s3']['object']['key']
+    if verbose:
+        print("S3 info: " + S3WatchBucket + " " + S3WatchFile)
+        
+    CTMSecret = str("CTMS3Watch-" + S3WatchBucket)
+    
+    secret_response = secretsclient.get_secret_value(SecretId=CTMSecret)
+    ctminfo = secret_response['SecretString']
+    ctmData = json.loads(ctminfo)
+    CTMEnvironment = ctmData['CTMEnvironment']
+    CTMServer = ctmData['CTMServer']
+    ReqType = ctmData['Putaction']
+    if ReqType == 'addevent':
+        CTMEvent = ctmData['CTMEvent']
+        CTMOdate = ctmData['CTMOdate']
+    if ReqType == 'runjob':
+        Jobname = ctmData['Jobname']
+        Folder = ctmData['Folder']
+    if ReqType == 'createjob':
+        Jobname = ctmData['Jobname']
+        Folder = ctmData['Folder']
+        Template = ctmData['Template']
+        
+    ctmrequest_args = collections.namedtuple('Ctmrequest_Args', ['CTMEnvironment', 'CTMServer', 'ReqType', 'Jobname', 'Folder', 'Template', 'CTMEvent', 'CTMOdate', 'S3WatchBucket', 'S3WatchFile'])
+    ctmreq = ctmrequest_args(CTMEnvironment, CTMServer, ReqType, Jobname, Folder, Template, CTMEvent, CTMOdate, S3WatchBucket, S3WatchFile)
+        
+    return ctmreq
+    
+#
+#------------------------------------------------------------------------------------------------
+#   do_request performs the Control-M request that is the result of an SQS, SNS or S3 event
+#   Request parameters set up in source-specific routines
+#       CTM Environment: 
+#       CTMServer:
+#       Request Type:
+#       Job:
+#       Folder:
+#       Job Template:
+#       Event:
+#       Order Date:
+#------------------------------------------------------------------------------------------------
+#
+def do_request(ctmreq):
+    CTMEnvironment = ctmreq.CTMEnvironment
+    CTMServer = ctmreq.CTMServer
+    ReqType = ctmreq.ReqType
+    Jobname = ctmreq.Jobname
+    Folder = ctmreq.Folder
+    Template = ctmreq.Template
+    CTMEvent = ctmreq.CTMEvent
+    CTMOdate = ctmreq.CTMOdate
+    S3WatchBucket = ctmreq.S3WatchBucket
+    S3WatchFile = ctmreq.S3WatchFile
     auth = build_auth(CTMEnvironment)
     ctmtoken = ctm_login(auth)
     
     if ReqType == 'runjob':
-        run_ctm_job(ctmtoken, auth.baseurl, CTMServer, Jobname, Folder)
+        ctmresponse = run_ctm_job(ctmtoken, auth.baseurl, CTMServer, Jobname, Folder)
     elif ReqType == 'createjob':
-        Template = msgpayload['Request'][0]['RequestAttributes']['Template']
-        template_json = get_job_template(Template, auth.bucket, Folder, Jobname)
-        create_ctm_jobfile(template_json, ctmtoken, auth.baseurl)
+        template_json = get_job_template(Template, auth.bucket, Folder, Jobname, S3WatchBucket, S3WatchFile)
+        ctmresponse = ctm_create_job(template_json, ctmtoken, auth.baseurl)
+    elif ReqType == 'addevent':
+        ctmresponse = ctm_addevent(ctmreq, ctmtoken, auth.baseurl)
         
-    ctm_logout(ctmtoken, auth)
-        
+    response = ctm_logout(ctmtoken, auth)
+    
     return response
-	
-def sns_event(event):
-
-    return
-	
-def s3_event(event):
-
-    return
-
+    
 def build_auth(CTMEnvironment):
     CTMSecret = str("CTMEnvironment_" + CTMEnvironment)
     secret_response = secretsclient.get_secret_value(SecretId=CTMSecret)
@@ -79,7 +170,7 @@ def build_auth(CTMEnvironment):
     username = ctmData['username']
     password = ctmData['password']
     bucket = ctmData['bucket']
-    baseurl = 'https://' + ctmEndpoint + ':8443/automation-api/'
+    baseurl = ctmEndpoint
     login_args = collections.namedtuple('Login_Args', ['baseurl', 'username', 'password', 'bucket'])
     auth = login_args(baseurl, username, password, bucket)
     return auth  # return auth info as named tuple
@@ -134,7 +225,7 @@ def ctm_login(auth):
         print('Token: ' + token)
 
     return token  # return the token
-	
+    
 def ctm_logout(token, auth, exit=0):
     # if logged in, need to call logout before quiting to invalidate the token for security
     # this prevents the chance of intercepting a token and being reused later
@@ -156,8 +247,8 @@ def ctm_logout(token, auth, exit=0):
     if verbose:
         print(r4.text)
         
-    quit(exit)
-	
+    return exit
+    
 def run_ctm_job(ctmtoken, baseurl, CTMServer, Jobname, Folder):
     global verbose
     response = 200
@@ -168,8 +259,8 @@ def run_ctm_job(ctmtoken, baseurl, CTMServer, Jobname, Folder):
     if Jobname == 'ALL':
         Jobname = '*'
     body = json.loads('{"folder":"' + Folder + '", "jobs":"' + Jobname + '", "ctm":"' + CTMServer +'"}')
-	
-	
+    
+    
     if verbose:
         print('Run job via URL: ' + rjoburl)
         print('Header: ' + json.dumps(hdrdata))
@@ -187,14 +278,13 @@ def run_ctm_job(ctmtoken, baseurl, CTMServer, Jobname, Folder):
         print(rcRunJob.status_code)
     
     return response
-	
-def create_ctm_jobfile(template_json, ctmtoken, baseurl):
+    
+def ctm_create_job(template_json, ctmtoken, baseurl):
     global verbose
-    response = 200
     rjoburl = baseurl + 'run'
     hdrdata = json.loads('{"Authorization": "Bearer ' + ctmtoken + '"}')
     jobJsonStr = json.dumps(template_json)
-    print("Json str: " + jobJsonStr)
+    #print("Json str: " + jobJsonStr)
     jobJsonStream = io.StringIO(jobJsonStr)
     jobJsonFile = [('jobDefinitionsFile', ('jobs.json', jobJsonStream, 'application/json'))]
     r = requests.post(rjoburl, files=jobJsonFile, headers=hdrdata, verify=verify_certs)
@@ -204,13 +294,19 @@ def create_ctm_jobfile(template_json, ctmtoken, baseurl):
 
     return r.status_code
 
-def get_job_template(template, bucket, Folder, Jobname):
-    KEY = template + ".template"         # Construct S3 Key
-    content_object = s3res.Object(bucket, KEY)
+def get_job_template(template, bucket, Folder, Jobname, S3WatchBucket, S3WatchFile):
+    content_object = s3res.Object(bucket, template)
+    if verbose:
+        print("get_job_template parms: " + template + " " + bucket + " " + Folder + " " + Jobname + " " + S3WatchBucket + " " + S3WatchFile)
     template_str = content_object.get()['Body'].read().decode('utf-8')
-    #template_str = str(json.loads(template_json))
-    template_str = template_str.replace('$$FOLDER$$', Folder)
-    template_str = template_str.replace('$$JOBPFX$$', Jobname)
+    if Folder != 'none':
+        template_str = template_str.replace('VAR_FOLDER_VAR', Folder)
+    if Jobname != 'none' and Jobname != 'ALL':
+        template_str = template_str.replace('VAR_JOBPFX_VAR', Jobname)
+    if S3WatchBucket != 'none':
+        template_str = template_str.replace('VAR_S3BUCKET_VAR', S3WatchBucket)
+    if S3WatchFile != 'none':
+        template_str = template_str.replace('VAR_S3FILE_VAR', S3WatchFile)
     
     template_json = json.loads(template_str)
     
