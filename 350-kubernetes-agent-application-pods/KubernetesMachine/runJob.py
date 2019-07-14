956a20dace7c84488a383c1731f510777866d6e3
Joe Goldberg <joe_goldberg@bmc.com>
2019-07-14 09:52:03 -0400

    Added backoffLimit

--- 350-kubernetes-agent-application-pods/KubernetesMachine/runJob.py
+++ 350-kubernetes-agent-application-pods/KubernetesMachine/runJob.py
@@ -1,12 +1,13 @@
 
 from os import path
-from kubernetes import client, config, utils
+from kubernetes import config
+import kubernetes.client, kubernetes.utils
 
 import getopt, os, sys, time
 import signal
 
 kNameSpace = "default"
-stime = 15				# Default sleep interval for status check
+stime = 15                              # Default sleep interval for status check
 
 def main(argv):
    global kJobname, kNameSpace, podName
@@ -26,30 +27,33 @@ def main(argv):
 
    print("Processing job: " + kJobname)
    startJob(kJobname)
-   
+
    signal.signal(signal.SIGTERM, termSignal)
-   
-   jobStatus = status(kJobname)
-   
+
+   jobStatus, podsActive, podsSucceeded, podsFailed = status(kJobname)
+
    podName = listPod(kJobname)
    getLog(podName)
-      
+
+   print("Pods Statuses: %s Running / %s Succeeded / %s Failed" % (podsActive, podsSucceeded, podsFailed))
+   print("Job Completion status: " + jobStatus)
+   
    deleteJob(kJobname, podName)
    
-   quit(0)
+   sys.exit(jobStatus)
    
 def startJob(kJobname):
    yaml = kJobname + ".yaml"
    config.load_kube_config()
-   k8s_client = client.ApiClient()
-   k8s_api = utils.create_from_yaml(k8s_client, yaml)
-   
+   k8s_client = kubernetes.client.ApiClient()
+   k8s_api = kubernetes.utils.create_from_yaml(k8s_client, yaml)
+
    try:
       kJob = k8s_api.read_namespaced_job(kJobname, kNameSpace)
    except:
       print("Failed creating job: " + kJobname)
       sys.exit(2)
-	  
+
    print("Job {0} created".format(kJob.metadata.name))
    return
    
@@ -58,31 +62,48 @@ def status(kJobname):
    jobRunning = "Running"
    podLabelSelector = 'job-name=' + kJobname
    config.load_kube_config()
-   batchV1 = client.BatchV1Api()
-   time.sleep(5)                  # Give the Pod a chance to initialize
-   
+   batchV1 = kubernetes.client.BatchV1Api()
+   time.sleep(stime)                  # Give the Pod a chance to initialize
+
    while jobRunning == "Running":
       try:
          ret = batchV1.list_namespaced_job(kNameSpace, label_selector=podLabelSelector)
       except:
-         print("Failed getting job status: " + kJobname)
-         sys.exit(4)
-		 
+             print("Failed getting job status: " + kJobname)
+             sys.exit(4)
+
       for i in ret.items:
          jobStatus = str(i.status.active)
-         print("Kubernetes Job Status :" + jobStatus)
-      if jobStatus == "1":
-         jobRunning = "Running"
-         time.sleep(stime)
+         podsFailed = str(i.status.failed)
+         podsSucceeded = str(i.status.succeeded)
+      if jobStatus.isdigit():
+         if jobStatus >= "1":
+            jobRunning = "Running"
+            time.sleep(stime)
+         else:
+            jobRunning = "Not Running"
       else:
          jobRunning = "Not Running"
-	  
-   print("Job ended execution")
-   return jobStatus
-   
+
+   podsFailed = str(i.status.failed)
+   podsSucceeded = str(i.status.succeeded)
+   podsActive = str(i.status.active)
+   if podsActive == "None":
+      podsActive = "0"
+   if podsSucceeded == "None":
+      podsSucceeded = "0"
+   if podsFailed == "None":
+      podsFailed = "0"
+      if podsSucceeded >= 1:
+         jobStatus = "0"
+   else:
+      jobStatus = "1"
+    
+   return jobStatus, podsActive, podsSucceeded, podsFailed
+
 def listPod(kJobname):
    config.load_kube_config()
-   coreV1 = client.CoreV1Api()
+   coreV1 = kubernetes.client.CoreV1Api()
    podLabelSelector = 'job-name=' + kJobname
    print("Listing pod for jobname:" + kJobname)
    ret = coreV1.list_namespaced_pod(kNameSpace, label_selector=podLabelSelector)
@@ -92,36 +113,36 @@ def listPod(kJobname):
           (i.metadata.name))
    return podName
 
-  
+
 def getLog(podName):
    config.load_kube_config()
-   coreV1 = client.CoreV1Api()
+   coreV1 = kubernetes.client.CoreV1Api()
    ret = coreV1.read_namespaced_pod_log(podName, kNameSpace)
    print("Log output from Pod: " + podName)
    print(ret)
    return
-   
+
 def deleteJob(kJobname, podName):
    config.load_kube_config()
-   jobBody = client.V1Job()
-   batchV1 = client.BatchV1Api()
+#  jobBody = kubernetes.client.V1Job()
+   batchV1 = kubernetes.client.BatchV1Api()
    ret = batchV1.delete_namespaced_job(kJobname, kNameSpace)
    print("Job deleted: " + kJobname)
    
-   podBody = client.V1DeleteOptions()
-   coreV1 = client.CoreV1Api()
+   podBody = kubernetes.client.V1DeleteOptions()
+   coreV1 = kubernetes.client.CoreV1Api()
    ret = coreV1.delete_namespaced_pod(podName, kNameSpace)
    print("Pod deleted: " + podName)
-   
+
    return
 
-def termSignal(signalNumber, frame):  
+def termSignal(signalNumber, frame):
    global kJobname, kNameSpace, podName
-   print("Terminating due to SIGTERM: %i" % signalNumber)
+   print("Terminating due to SIGTERM: " + signalNumber)
    podName = listPod(kJobname)
    getLog(podName)
    deleteJob(kJobname, podName)
    sys.exit(8)
-   
+
 if __name__ == '__main__':
    main(sys.argv[1:])
