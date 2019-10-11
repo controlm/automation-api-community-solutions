306aab0cadf86405b061a509fdddd08f7c70cd34
Michiel Tiller <michiel@tiller.nl>
2019-10-11 16:48:16 +0200

    add Apigee Edge Gateway examples

---
+++ 7-api-gateway-integrations/apigee-outbound-auth/apiproxy/resources/jsc/api-config.js
@@ -0,0 +1,11 @@
+/**
+ * Sets Automation API endpoint login credentials and url
+ */
+context.setVariable('control-m.automationApi', {
+  // Control-M Automation API credentials
+  username: 'youruser',
+  password: 'password',
+
+  // Automation API endPoint URL:
+  baseUrl: 'https://ENDPOINT:8443/automation-api',
+});
