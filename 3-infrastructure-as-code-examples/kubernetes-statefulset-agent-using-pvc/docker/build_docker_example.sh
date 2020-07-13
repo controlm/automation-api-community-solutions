1277e223d722e4dcf1a190a6444dc3a950ba0e9e
ybergman <ybergman@bmc.com>
2020-07-13 13:13:12 +0300

    correct file name and job name
    Correct the name of the k8s job name in controlm job .
    Change Control-M job name

--- 3-infrastructure-as-code-examples/kubernetes-statefulset-agent-using-pvc/docker/build_docker_example.sh
+++ 3-infrastructure-as-code-examples/kubernetes-statefulset-agent-using-pvc/docker/build_docker_example.sh
@@ -1,16 +1,9 @@
-# This example is running the build command with parameters, login to aws and push the image to ECR .
+# This is a build command example with parameters
 # You need to change the parameters: 
-#   1. image tag - compose of repo name: <number>.dkr.ecr.<region>.amazonaws.com/<name>:<image-tag>
 #   1. endpoint, 
 #   2. user, 
 #   3. password
 #   4. the agent image you want to install (taken from "ctm provision images Linux" cli)
-sudo docker build -t 000000000000.dkr.ecr.us-west-2.amazonaws.com/repo-name:agent-tag --build-arg AAPI_END_POINT=https://endpoint-hostname:8443/automation-api --build-arg AAPI_USER=emuser --build-arg AAPI_PASS=password --build-arg AGENT_IMAGE_NAME=Agent_Image.Linux  . 
+$sudo docker build --build-arg AAPI_END_POINT=https://<endpoint>:8443/automation-api --build-arg AAPI_USER=user --build-arg AAPI_PASS=password --build-arg AGENT_IMAGE_NAME=Agent_Image.Linux  . 
 
-# aws login
-sudo `aws ecr get-login --no-include-email`
-
-# push tagged image to ECR
-sudo docker push 000000000000.dkr.ecr.us-west-2.amazonaws.com/repo-name:agent-tag
-
-# later on, the statefulset will use this image from ECR
+# Don't forget to upload the result to your docker repository (ECR, DockerHub, etc.) for k8s use.
