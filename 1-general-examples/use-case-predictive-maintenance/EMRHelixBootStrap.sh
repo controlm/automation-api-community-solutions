b7f80bdd02b3ee8d04907fc7500338209fb7d120
Joe Goldberg <joe_goldberg@bmc.com>
2021-03-31 09:37:42 -0400

    Clean up infromation

--- 1-general-examples/use-case-predictive-maintenance/EMRHelixBootStrap.sh
+++ 1-general-examples/use-case-predictive-maintenance/EMRHelixBootStrap.sh
@@ -9,7 +9,7 @@ curl --silent --location https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum -y install nodejs
 wget https://bmc-preprod-saas-agent-application-artifacts.s3.us-west-2.amazonaws.com/extracted/deployment315/root/apps/DEV/9.0.20.040/ctm-cli.tgz
 sudo npm install -g ctm-cli.tgz
-ctm env saas::add ctm https://se-americas-prod-aapi.prod.us1.preprod.ctmsaas.com/automation-api "UFBSR0JYOmJkM2M0MzdjLThhNTUtNDhjYi04Mjk5LTJiMzQxZTJkMjViOTp4aEF2T05URHlDNnJzUVNrODAwV1lqRFNFU2pwakVRcmdGajl1UUcyTVNzPQ=="
+ctm env saas::add ctm <Automation API endpoint> "<API token"
 ctm env set ctm
 ctm provision saas::install Agent_Amazon.Linux JGO_GCPAgents $ALIAS
 ctm provision image Hadoop_plugin.Linux
