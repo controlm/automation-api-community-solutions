48facc80c01ff7d0e4c30957b2c339449113c1a3
Joe Goldberg <joe_goldberg@bmc.com>
2022-02-27 19:20:59 -0500

    wip- dynamic agents

--- 3-infrastructure-as-code-examples/manage-workload-on-ec2/part2-launch-instance.sh
+++ 3-infrastructure-as-code-examples/manage-workload-on-ec2/part2-launch-instance.sh
@@ -5,5 +5,3 @@ aws ec2 describe-images --region us-west-2 --image-ids ami-08c703f67c7b7f793 --q
 
 #   Launch ec2 instance(s) 
 aws ec2 run-instances --image-id ami-08c703f67c7b7f793 --count 1 --region us-west-2 --instance-type t2.micro --key-name JoGoKey --security-group-ids sg-0125e6ff1d318ef2a --iam-instance-profile Name=jogoldbeAdmin4EC2 --tag-specifications 'ResourceType=instance,Tags=[{Key=ctmenvironment,Value=ctmprod},{Key=ctmserver,Value=smprod},{Key=ctmhostgroup,Value=awsec2group},{Key=Name,Value=ctmprod Control-M Agent}]'
-
-aws
