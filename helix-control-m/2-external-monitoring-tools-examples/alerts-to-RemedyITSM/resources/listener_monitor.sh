08210b0d687cb5559f7dd0c45a62dd7458b5b5f1
dcompane <dcompane@gmail.com>
2023-02-19 17:31:28 -0800

    Miscellaneous changes

--- helix-control-m/2-external-monitoring-tools-examples/alerts-to-RemedyITSM/resources/listener_monitor.sh
+++ helix-control-m/2-external-monitoring-tools-examples/alerts-to-RemedyITSM/resources/listener_monitor.sh
@@ -3,14 +3,14 @@ set -x
 
 source ~/.bash_profile
 
-# add with crontab -e
+# add to a job (check your licensed executions) or to crontab for the user
 #*/5 * * * * extalerts/listener_monitor.sh
 # note that the environment must be sourced.
 #crontab -l to see that it is set
 
 status=`ctm run alerts:stream::status | grep 'status":'| awk -F '"' '{print $4}'`
 ctmenv=`ctm env show | grep -i "current environment" | awk '{print $3}'`
-echo `date` "- Alerts Listener for $ctmenv on $(hostname -f) are being tested. Result $status" >> /tmp/listener_monitor.log
+echo `date` "- Alerts Listener for $ctmenv on $(hostname -f) is being tested. Result $status" >> /tmp/listener_monitor.log
 
 while [ ".$status" != ".OK" ]; do
    ctm run alerts:listener::stop
