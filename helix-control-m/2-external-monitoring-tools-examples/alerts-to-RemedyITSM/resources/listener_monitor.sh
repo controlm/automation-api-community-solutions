c999dcd725e15b57cef2f3c9bdb255821f1fb1e3
dcompane <dcompane@gmail.com>
2023-02-19 16:08:58 -0800

    fixes for listener_monitor.sh

--- helix-control-m/2-external-monitoring-tools-examples/alerts-to-RemedyITSM/resources/listener_monitor.sh
+++ helix-control-m/2-external-monitoring-tools-examples/alerts-to-RemedyITSM/resources/listener_monitor.sh
@@ -3,23 +3,23 @@ set -x
 
 source ~/.bash_profile
 
-
-#crontab -l
+# add with crontab -e
 #*/5 * * * * extalerts/listener_monitor.sh
-# script does not finish... the script remains hanging and must be killed
-
+# note that the environment must be sourced.
+#crontab -l to see that it is set
 
 status=`ctm run alerts:stream::status | grep 'status":'| awk -F '"' '{print $4}'`
 ctmenv=`ctm env show | grep -i "current environment" | awk '{print $3}'`
-echo `date` "- Alerts Listener for $ctmenv on $(hostname -f) wre being tested. Result $status" >> /tmp/listener_monitor.log
+echo `date` "- Alerts Listener for $ctmenv on $(hostname -f) are being tested. Result $status" >> /tmp/listener_monitor.log
 
 while [ ".$status" != ".OK" ]; do
    ctm run alerts:listener::stop
    echo `date` "- Alerts Listener for $ctmenv on $(hostname -f) was terminated." >> /tmp/listener_monitor.log
-   # mailx -s "Alerts Listener for $ctmenv on $(hostname -f) was terminated" dcompane@bmc.com <<EOF
+   # mailx -s "Alerts Listener for $(ctm env show | grep -i "current environment" | awk '{print $3}') on $(hostname -f) was terminated" dcompane@bmc.com <<EOF
 # No message
 # EOF
    sleep 30
    status=`ctm run alerts:stream::status | grep 'status":'| awk -F '"' '{print $4}'`
 done
+
 
