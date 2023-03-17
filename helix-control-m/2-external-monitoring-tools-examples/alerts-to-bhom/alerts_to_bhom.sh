6c000b10f0d47508eba46e7d7baaee9a4bf5fb43
davidctm <77340645+davidctm@users.noreply.github.com>
2023-03-18 00:57:59 +0100

    script loop update

--- helix-control-m/2-external-monitoring-tools-examples/alerts-to-bhom/alerts_to_bhom.sh
+++ helix-control-m/2-external-monitoring-tools-examples/alerts-to-bhom/alerts_to_bhom.sh
@@ -25,40 +25,33 @@ sev_R="MINOR"
 # Send updates of existing alerts? (Y/N)
 alert_updates="Y"
 
+# Declare arrays with the HCTM alert fields and BHOM slot names - DO NOT MODIFY
+alert_fields=("eventType" "id" "server" "fileName" "runId" "severity" "status" "time" "user" "updateTime" "message" "runAs" "subApplication" "application" "jobName" "host" "type" "closedByControlM" "ticketNumber" "runNo" "notes")
+bhom_slots=("eventType" "alertId" "ctmServer" "fileName" "runId" "severity" "alertStatus" "creation_time" "ctmUser" "updateTime" "msg" "runAs" "subApplication" "application" "jobName" "source_hostname" "alertType" "closedByControlM" "ticketNumber" "runNo" "alertNotes")
+
+# If alert updates are not needed, exit if "call_type" = "U"
 if [ $alert_updates == "N" ] ; then
-   # If this is an alert update ("eventType" = U), exit
    if [ $2 == "U" ] ; then exit 0 ; fi
 fi
 
-# Declare array with all the alert field names (DO NOT MODIFY - using default field names for HCTM alerts)
-field_names=("eventType" "id" "server" "fileName" "runId" "severity" "status" "time" "user" "updateTime" "message" "runAs" "subApplication" "application" "jobName" "host" "type" "closedByControlM" "ticketNumber" "runNo" "notes")
-
-# START PROCESSING ALERT DATA
-
-# Start JSON and add BHOM mandatory fields
+# Start JSON and add first BHOM slots
 hctm_tenant=`echo $hctm_url | cut -c 9-`
-json_data="[ { \"class\" : \"$bhom_class\", \"location\" : \"$hctm_name\", \"source_identifier\" : \"$hctm_tenant\","
+json_data="[ { \"class\" : \"$bhom_class\", \"location\" : \"$hctm_name\", \"source_identifier\" : \"$hctm_tenant\""
 
 # Start creating url for the job link
 job_link=$hctm_url/ControlM/#Neighborhood:
 
-# Convert parameters to JSON format
-num_fields=${#field_names[@]}
-for i in ${!field_names[@]}; do
-   field=${field_names[$i]}
-   next_field=${field_names[$i+1]}
+# START PROCESSING ALERT DATA
+num_fields=${#alert_fields[@]}
+for (( i=0; i<=$((num_fields-1)); i++ )) ; do
+   field=${alert_fields[$i]}
+   next_field=${alert_fields[$i+1]}
    if [ $i != $((num_fields-1)) ] ; then
-      value=`echo $* | grep -oP "(?<=${field}: ).*(?= ${next_field}:)"`
+      value=`echo $* | grep -oP "(?<=\b${field}\b: ).*?(?= \b${next_field}\b:)"`   
       
       # Update some fields for BHOM compatibility
       case $field in
-         id)
-            # Change "id" field name to "alertId"
-            field="alertId"
-         ;;
          server)
-            # Change "server" field name to "ctmServer"
-            field="ctmServer"
             # Save "server" value in a variable (for the job link)
             ctm_server=$value
          ;;
@@ -71,24 +64,10 @@ for i in ${!field_names[@]}; do
             bhom_severity="sev_${value}"
             value=${!bhom_severity}
          ;;
-         status)
-            # Change "status" field name to "alertStatus"
-            field="alertStatus"
-         ;;
          time | updateTime)
-            # Change "time" field name to "creation_time"
-            if [ $field == "time" ] ; then field="creation_time" ; fi
             # Convert to Epoch time, in milisecs
             D="$value"
             value=`date -d "${D:0:8} ${D:8:2}:${D:10:2}:${D:12:2} +0000" "+%s%3N"`
-         ;;
-         user)
-            # Change "user" field name to "ctmUser"
-            field="ctmUser"
-         ;;
-         message)
-            # Change "message" field name to "msg"
-            field="msg"
          ;;
          jobName)
             # Add "jobName" to the final job link
@@ -96,32 +75,27 @@ for i in ${!field_names[@]}; do
             job_link=$job_link"&name="$job_name"&direction=1&radius=3"  # Direction and radius can be customized
          ;;
          host)
-            # Change "host" field name to "source_hostname"
-            field="source_hostname"
             # Save "host" value in a variable (used to determine whether to include the job link)
             saved_host=$value
          ;;
-         type)
-            # Change "type" field name to "alertType"
-            field="alertType"
-         ;;
       esac
-      text=`echo -ne "\"$field\" : \"$value\","`
 
    else
-      # If last field ("notes"), capture until EOL
-      value=`echo $* | grep -oP "(?<=${field}: ).*(?)"`
-      # Change "notes" field name to "alertNotes"
-      text=`echo -ne "\"alertNotes\" : \"$value\""`
-      # Add link to job (only if "host" was not empty, meaning it is an alert related to a job)
-      if [ ! -z "$saved_host" ] ; then
-         text=$text", \"jobLink\" : \"$job_link\""
-      fi  
-      # Close JSON
-      text=$text" } ]"
+      # If last field, capture until EOL
+      value=`echo $* | grep -oP "(?<=\b${field}\b: ).*"`
    fi
-   json_data="$json_data $text"
+   slot_name=${bhom_slots[$i]}
+   text=", \"$slot_name\" : \"$value\""
+   json_data=$json_data$text
 done
+
+# Add link to the problematic job (only if "host" was not empty, meaning it is an alert related to a job)
+if [ ! -z "$saved_host" ] ; then
+   json_data=$json_data", \"jobLink\" : \"$job_link\""
+fi      
+
+# Close JSON
+json_data=$json_data" } ]"
 
 # Set library path to solve curl error when Helix Control-M Agent is installed
 # USE ONLY if you get the error: "curl: (48) An unknown option was passed in to libcurl"
