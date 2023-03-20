28e3c77783ef9b53dfbeb8c5314d1edba1218e37
dcompane <dcompane@gmail.com>
2023-03-20 13:44:08 -0700

    monitor the listener

--- helix-control-m/2-external-monitoring-tools-examples/alerts-to-helix-itsm/resources/Windows/listener_init.bat
+++ helix-control-m/2-external-monitoring-tools-examples/alerts-to-helix-itsm/resources/Windows/listener_init.bat
@@ -1,28 +1,48 @@
-echo NOTE: ENSURE YOU ADDD THE FULL PATH TO ALL THE FILES THAT NEED IT
+echo NOTE: ENSURE YOU ADD THE FULL PATH TO ALL THE FILES THAT NEED IT
 
-echo Setting environment to olas
-call ctm env set olas
+echo on
 
-echo Setting up External Alert management for environment olas
-call ctm run alerts:listener:environment::set olas
+REM ensure you use the fqdn of your tenant for aapi
+REM    you can find that information on the configuration section
+REM    of the Automation API 
+set env_name=%1
+set host_name=%2
+set "token=%3"
 
-echo NOTE: ENSURE YOU ADDD THE FULL PATH TO ALL THE FILES THAT NEED IT like here!
+echo Creating the environment
+call ctm environment delete %env_name%
+call ctm environment saas::add %env_name% https://%hostname%/automation-api %token%
+
+echo on
+echo Setting environment to %env_name%
+call ctm env set %env_name%
+
+echo on
+echo Setting up External Alert management for environment %env_name%
+call ctm run alerts:listener:environment::set %env_name%
+
+echo on
+echo NOTE: ENSURE YOU ADD THE FULL PATH TO ALL THE FILES THAT NEED IT like here!
 echo Setting up External Alert management script to extalert.cmd
-call ctm run alerts:listener:script::set %CD%\..\..\extalert.cmd
+call ctm run alerts:listener:script::set "C:\alerts-to-helix-itsm\extalert.cmd"
 
-echo NOTE: ENSURE YOU ADDD THE FULL PATH TO ALL THE FILES THAT NEED IT like here!
+echo on
+echo NOTE: ENSURE YOU ADD THE FULL PATH TO ALL THE FILES THAT NEED IT like here!
 echo Setting up External Alert management template to field_names.json
-call ctm run alerts:stream:template::set -f %CD%\..\..\field_names.json
+call ctm run alerts:stream:template::set -f "C:\alerts-to-helix-itsm\field_names.json"
 
+echo on
 echo Enabling External Alerts on BMC Helix Control-M
-call ctm config systemsettings::set -f %CD%\..\..\systemsettings.json
+call ctm config systemsettings::set "C:\alerts-to-helix-itsm\systemsetting.json"
 
-echo External Alerts status
-call ctm run alerts:stream::status
-
+echo on
 echo Closing External Alerts stream, if open
 call ctm run alerts:stream::close true
 
+echo on
+echo External Alerts status
+call ctm run alerts:stream::status
+
+echo on
 echo Explicitly opening  External Alerts stream
 call ctm run alerts:stream::open
-
--- helix-control-m/2-external-monitoring-tools-examples/alerts-to-helix-itsm/resources/Windows/listener_monitor.bat
+++ helix-control-m/2-external-monitoring-tools-examples/alerts-to-helix-itsm/resources/Windows/listener_monitor.bat
@@ -2,43 +2,80 @@ echo off
 
 set cycle=300
 
-date /T >> %TEMP%\listener_alerts.log
-time /T >> %TEMP%\listener_alerts.log
-echo "Starting the checking cycle" >> %TEMP%\listener_alerts.log
+set filedate=%date:/=-%
+set filedate=%filedate:~4,10%
 
-:Start
-
-date /T >> %TEMP%\listener_alerts.log
-time /T >> %TEMP%\listener_alerts.log
-echo "Entered the cycle" >> %TEMP%\listener_alerts.log
-
-call ctm run alerts:stream::status > %TEMP%\statustmp.tmp
-findstr status %TEMP%\statustmp.tmp > %TEMP%\status.tmp
-set /p statustmp=<%TEMP%\status.tmp
-set status=%statustmp:~13,2%
-
-type %TEMP%\statustmp.tmp >> %TEMP%\listener_alerts.log
-
-if NOT .%status% == .OK  (
-   set cycle=30
-   echo "Stoping service, just in case it is alive" >> %TEMP%\listener_alerts.log
-   call ctm run alerts:listener::stop 2>&1 >> %TEMP%\listener_alerts.log
-   echo "Starting service" >> %TEMP%\listener_alerts.log
-   call ctm run alerts:listener::start 2>&1 >> %TEMP%\listener_alerts.log
-   echo "Verifying service" >> %TEMP%\listener_alerts.log
-   call ctm run alerts:stream::status 2>&1 >> %TEMP%\listener_alerts.log
+REM Set parameter to Y if you want log to file in the % TEMP % directory 
+REM TEMP directory depends on the user. It is C:\Windows\TEMP for local system account.
+if .%1 == .Y (
+	set filedate=%date:/=-%
+	set filedate=%filedate:~4,10%
+    set "log_dev=>>%TEMP%\listener_alerts-%filedate%.log"
+	set "log_message=file %TEMP%\listener_alerts-%filedate%.log"
 ) else (
-   set cycle=300
+	set log_dev=
+	set log_message=StdOut
 )
 
-del %TEMP%\statustmp.tmp
-del %TEMP%\status.tmp
+REM Not logging this to %log_dev% so it will be visible for user.
+echo Logging to %log_message%
 
+echo Entered the script at %date% %time% %log_dev%
 
-echo "Entering wait for next cycle (%cycle% seconds)" >> %TEMP%\listener_alerts.log
-timeout /T %cycle% /NOBREAK > NUL
+:start
+	echo Start cycle at %date% %time% %log_dev%
+	echo Verifying alert listener status %log_dev%
+	call ctm run alerts:stream::status > %TEMP%\statustmp.tmp
+	type %TEMP%\statustmp.tmp %log_dev%
 
-goto Start
+	findstr status %TEMP%\statustmp.tmp > %TEMP%\status.tmp
+	set /p status=<%TEMP%\status.tmp
+	set status=%status:~13,2%
 
-echo "Exiting cycle" >> %TEMP%\listener_alerts.log
+	if NOT .%status% == .OK  (
+		echo Status was found to be not OK %log_dev%
+		REM if logging to file, log also to StdOut for the job to capture the entry
+		if .%1 == .Y (
+			echo Status was found to be not OK at %date% %time% 
+		)
+		echo Stoping the listener, just in case it is alive %log_dev% 
+		REM More dramatically, the stream could be force closed and opened
+		REM     call ctm run alerts:stream::close true 2>&1 %log_dev%
+		REM     call ctm run alerts:stream::open 2>&1 %log_dev%
+		REM		NOTE: stream::open is not needed, as listener::start opens the stream
+		call ctm run alerts:listener::stop 2>&1 %log_dev%
+		echo Starting the listener %log_dev%
+		call ctm run alerts:listener::start 2>&1 %log_dev%
+		REM The verification below is for display only. 
+		REM		Status for action is not verified until next cycle.
+		REM		This is why on failure, the next verification is in 30 seconds
+		echo Verifying the listener after restart %log_dev%
+		call ctm run alerts:stream::status 2>&1 %log_dev% 
+		REM wait only for 30 seconds to retest
+		set cycle=30
+	) else (
+		REM wait 300 seconds to retest
+		set cycle=300
+	)
+	
+	del %TEMP%\statustmp.tmp
+	del %TEMP%\status.tmp
+
+	set now=%time::=% 
+	set now=%now:.=% 
+	if  %now% GTR 23550000 (
+		echo Terminating the cycle at end of day at %time% (now=%now%) %log_dev%
+	) else (	
+		echo Entering wait for next cycle (%cycle% seconds) %log_dev%
+		timeout /T %cycle% /NOBREAK > NUL
+		goto start
+	)
+	
+:end
+
+REM if logging to file, log also to StdOut for the job to capture the entry
+echo Exiting cycle at %date% %time% %log_dev%
+if .%1 == .Y (
+	echo Exiting cycle at %date% %time% 
+)
 :the_end
